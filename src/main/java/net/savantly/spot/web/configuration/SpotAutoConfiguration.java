1450bf94a5f59568f7b104b82f2caf8656c891b8
Jeremy <jdbranham@hotmail.com>
2018-02-15 18:14:04 -0600

    changed content item to use json

--- src/main/java/net/savantly/spot/web/configuration/SpotAutoConfiguration.java
+++ src/main/java/net/savantly/spot/web/configuration/SpotAutoConfiguration.java
@@ -1,26 +1,75 @@
 package net.savantly.spot.web.configuration;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.base.Strings;
+
+import net.savantly.sprout.core.content.contentField.ContentField;
+import net.savantly.sprout.core.content.contentField.ContentFieldRepository;
+import net.savantly.sprout.core.content.contentItem.ContentItem;
+import net.savantly.sprout.core.content.contentItem.ContentItemRepository;
 import net.savantly.sprout.core.content.contentType.ContentType;
 import net.savantly.sprout.core.content.contentType.ContentTypeRepository;
+import net.savantly.sprout.core.content.fieldType.FieldType;
+import net.savantly.sprout.settings.AppSetting;
+import net.savantly.sprout.settings.SettingName;
+import net.savantly.sprout.settings.UISettings;
 
 @Configuration
 public class SpotAutoConfiguration {
 
-	public static final String LINK_LABEL_CONTENT_TYPE_KEY = "Link Label";
-	public static final String NODE_LABEL_CONTENT_TYPE_KEY = "Node Label";
+	public static final String GRAPH_LABELS = "Graph Labels";
+	public static final String GRAPH_LABELS_JSON = "GRAPH_LABELS_JSON";
+	public static final String GRAPH_LABELS_CONTENT = "GRAPH_LABELS_CONTENT";
 	
 	@Autowired
 	ContentTypeRepository contentTypes;
+	@Autowired
+	ContentItemRepository contentItems;
+	@Autowired
+	ContentFieldRepository contentFields;
+	@Autowired
+	UISettings settings;
 	
 	@PostConstruct
 	public void postConstruct() {
-		ensureContentTypeExist(NODE_LABEL_CONTENT_TYPE_KEY);
-		ensureContentTypeExist(LINK_LABEL_CONTENT_TYPE_KEY);
+		ensureContentTypeExist(GRAPH_LABELS);
+		ensureDefaultSettings();
+		ensureDefaultContentItemExists();
+	}
+
+	private void ensureDefaultContentItemExists() {
+		if(contentItems.findAllByContentType_id(GRAPH_LABELS).size() == 0) {
+			String defaultValue = "{\r\n" + 
+					"    \"node\": [\"SERVER\", \"APPLID\", \"VIRTUAL_MACHINE\"],\r\n" + 
+					"    \"link\": [\"CALLS\", \"MOUNTED_ON\", \"DEPENDS_ON\", \"ASSOCIATED_WITH\"]\r\n" + 
+					"}";
+			Map<ContentField, String> fieldValues = new HashMap<>();
+			fieldValues.put(contentFields.findOne(GRAPH_LABELS_JSON), defaultValue);
+			
+			ContentItem content = new ContentItem();
+			content.setName("Default graph labels");
+			content.setContentType(contentTypes.findOne(GRAPH_LABELS));
+			content.setId(GRAPH_LABELS_CONTENT);
+			content.setFieldValues(fieldValues);
+			contentItems.save(content);
+		}
+	}
+
+	private void ensureDefaultSettings() {
+		AppSetting siteName = settings.getSiteName();
+		if (siteName == null || Strings.isNullOrEmpty(siteName.getValue())) {
+			siteName = new AppSetting(SettingName.SITE_NAME, "EAI admin");
+			settings.save(siteName);
+		}
 	}
 
 	private void ensureContentTypeExist(String string) {
@@ -31,8 +80,23 @@ public class SpotAutoConfiguration {
 			contentType.setName(string);
 			contentType.setUpdateable(false);
 			contentType.setRequiresTemplate(false);
+			contentType.setFields(getFields(contentType));
 			contentTypes.save(contentType);
 		}
+	}
+
+	private Set<ContentField> getFields(ContentType contentType) {
+		ContentField field = new ContentField();
+		field.setFieldType(FieldType.json);
+		field.setContentType(contentType);
+		field.setId(GRAPH_LABELS_JSON);
+		field.setDisplayName(GRAPH_LABELS);
+		field.setRequired(true);
+		field.setName(GRAPH_LABELS);
+		field.setSortOrder(0);
+		HashSet<ContentField> set = new HashSet<ContentField>();
+		set.add(field);
+		return set;
 	}
 
 }
--- src/main/java/net/savantly/spot/web/modules/SpotController.java
+++ src/main/java/net/savantly/spot/web/modules/SpotController.java
@@ -1,15 +1,25 @@
 package net.savantly.spot.web.modules;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import net.savantly.spot.web.configuration.SpotAutoConfiguration;
+import net.savantly.sprout.core.content.contentField.ContentField;
+import net.savantly.sprout.core.content.contentField.ContentFieldRepository;
+import net.savantly.sprout.core.content.contentItem.ContentItem;
 import net.savantly.sprout.core.content.contentItem.ContentItemRepository;
 
 @RequestMapping("/rest/modules/spot")
@@ -18,22 +28,29 @@ public class SpotController {
 	
 	@Autowired
 	private ContentItemRepository contentItems;
+	@Autowired
+	private ContentFieldRepository contentFields;
+	ObjectMapper mapper = new ObjectMapper();
 
 	@RequestMapping(value="/labels")
-	public Map<String, List<String>> getLabels() {
-		List<String> linkLabels = new ArrayList<String>();
-		this.contentItems.findAllByContentType_id(SpotAutoConfiguration.LINK_LABEL_CONTENT_TYPE_KEY).forEach((o) -> {
-			linkLabels.add(o.getName());
-		});
-		List<String> nodeLabels = new ArrayList<String>();
-		this.contentItems.findAllByContentType_id(SpotAutoConfiguration.NODE_LABEL_CONTENT_TYPE_KEY).forEach((o) -> {
-			nodeLabels.add(o.getName());
-		});
+	public JsonNode getLabels() throws JsonProcessingException, IOException {
 
-		Map<String, List<String>> payload = new HashMap<>();
-		payload.put("node", nodeLabels);
-		payload.put("link", linkLabels);
-		return payload;
+		ContentField field = contentFields.findOne(SpotAutoConfiguration.GRAPH_LABELS_JSON);
+		
+		Comparator<ContentItem> cmp = Comparator.comparing(ContentItem::getLastModifiedDate);
+		Optional<ContentItem> latestContent = this.contentItems.findAllByContentType_id(SpotAutoConfiguration.GRAPH_LABELS).stream().max(cmp);
+		if (latestContent.isPresent()) {
+			Map<ContentField, String> fieldValues = latestContent.get().getFieldValues();
+			String json = fieldValues.get(field);
+			return mapper.readTree(json);
+		} else {
+			List<String> linkLabels = new ArrayList<String>();
+			List<String> nodeLabels = new ArrayList<String>();
+			Map<String, List<String>> payload = new HashMap<>();
+			payload.put("node", nodeLabels);
+			payload.put("link", linkLabels);
+			return mapper.convertValue(payload, JsonNode.class);
+		}
 	}
 	
 }
